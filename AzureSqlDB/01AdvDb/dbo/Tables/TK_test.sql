17ce7b5552fa6d3329d21fa595680a12a6fcffc5
HarveyHuBJ <tigerush80@hotmail.com>
2022-05-13 11:22:21 +0800

    add adventure works 2016 db

---
+++ AzureSqlDB/01AdvDb/dbo/Tables/TK_test.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TK_test] (
+    [RN]        BIGINT         NULL,
+    [rword]     VARCHAR (100)  NULL,
+    [position]  INT            NULL,
+    [rwordlen]  INT            NULL,
+    [tableName] VARCHAR (1000) NULL,
+    [LVL]       INT            NOT NULL
+);
+
---
+++ AzureSqlDB/01AdvDb/dbo/Tables/T_MyDemo1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[T_MyDemo1] (
+    [id]               INT            NULL,
+    [inserted]         NVARCHAR (100) NULL,
+    [ab]               INT            NULL,
+    [datedeleted]      DATETIME       NULL,
+    [nameChanged2baba] NVARCHAR (50)  NULL,
+    [f1]               NCHAR (10)     NULL
+);
+
