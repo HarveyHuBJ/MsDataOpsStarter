e83fb545b8988a11d952557ab003ef8c2f647098
HarveyHuBJ <tigerush80@hotmail.com>
2022-05-29 16:47:34 +0800

    LOAD data bulk insert:sas issue

--- AzureSqlDB/00AdminScripts/bulk_inserts.sql
+++ AzureSqlDB/00AdminScripts/bulk_inserts.sql
@@ -5,20 +5,20 @@
 -- #2 , mlbootcamp5_train.csv
 -- #3 , weights_heights.csv
 
-IF CHARINDEX('#1', '$(files)')>0 or '$(files)'=''
+IF CHARINDEX('#1', '$(files)')>0 or '$(files)'='all'
 BEGIN
     TRUNCATE TABLE Lab.telecom_churn
     BULK INSERT Lab.telecom_churn FROM '$(root)/telecom_churn.csv' WITH ( DATA_SOURCE = '$(name)BlobStorage' , FIELDTERMINATOR ='|' , ROWTERMINATOR ='|\n' ); 
 END
 
 BEGIN
-IF CHARINDEX('#2', '$(files)')>0 or '$(files)'=''
+IF CHARINDEX('#2', '$(files)')>0 or '$(files)'='all'
     TRUNCATE TABLE Lab.mlbootcamp5_train
     BULK INSERT Lab.mlbootcamp5_train FROM '$(root)/mlbootcamp5_train.csv' WITH ( DATA_SOURCE = '$(name)BlobStorage' , FIELDTERMINATOR ='|' , ROWTERMINATOR ='|\n' ); 
 END
 
 BEGIN
-IF CHARINDEX('#3', '$(files)')>0 or '$(files)'=''
+IF CHARINDEX('#3', '$(files)')>0 or '$(files)'='all'
     TRUNCATE TABLE Lab.weights_heights
     BULK INSERT Lab.weights_heights FROM '$(root)/weights_heights.csv' WITH ( DATA_SOURCE = '$(name)BlobStorage' , FIELDTERMINATOR ='|' , ROWTERMINATOR ='|\n' ); 
 END
