54c60d4c363a150b9e46fc2da4426490f81a2cdd
HarveyHuBJ <tigerush80@hotmail.com>
2022-05-29 17:37:14 +0800

    bulk insert: done!

--- AzureSqlDB/00AdminScripts/bulk_inserts.sql
+++ AzureSqlDB/00AdminScripts/bulk_inserts.sql
@@ -2,23 +2,25 @@
 
 -- category mappings
 -- #1 , telecom_churn.csv
--- #2 , mlbootcamp5_train.csv
--- #3 , weights_heights.csv
+-- #2 , weights_heights.csv
+
+SET NOCOUNT ON;
 
 IF CHARINDEX('#1', '$(files)')>0 or '$(files)'='all'
 BEGIN
-    TRUNCATE TABLE Lab.telecom_churn
+    PRINT 'Reloading [Lab].[telecom_churn] data.'
+    TRUNCATE TABLE Lab.telecom_churn;
     BULK INSERT Lab.telecom_churn FROM '$(root)/telecom_churn.csv' WITH ( DATA_SOURCE = '$(name)BlobStorage' , FORMAT = 'CSV',		FIELDTERMINATOR =',' ,	 firstrow=2 ,	ROWTERMINATOR ='0x0a' ); 
 END
+GO
 
-BEGIN
+  
 IF CHARINDEX('#2', '$(files)')>0 or '$(files)'='all'
-    TRUNCATE TABLE Lab.mlbootcamp5_train
-    BULK INSERT Lab.mlbootcamp5_train FROM '$(root)/mlbootcamp5_train.csv' WITH ( DATA_SOURCE = '$(name)BlobStorage' ,FORMAT = 'CSV',		FIELDTERMINATOR =',' ,	 firstrow=2 ,	ROWTERMINATOR ='0x0a' ); 
-END
-
 BEGIN
-IF CHARINDEX('#3', '$(files)')>0 or '$(files)'='all'
-    TRUNCATE TABLE Lab.weights_heights
+    PRINT 'Reloading [Lab].[weights_heights] data.'
+    TRUNCATE TABLE Lab.weights_heights;
     BULK INSERT Lab.weights_heights FROM '$(root)/weights_heights.csv' WITH ( DATA_SOURCE = '$(name)BlobStorage' , FORMAT = 'CSV',		FIELDTERMINATOR =',' ,	 firstrow=2 ,	ROWTERMINATOR ='0x0a'); 
 END
+
+PRINT 'Reloading completed.'
+
