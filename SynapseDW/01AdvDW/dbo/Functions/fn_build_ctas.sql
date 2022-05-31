5b44bcd896b4165a333beab789f91b7c96e83248
HarveyHuBJ <tigerush80@hotmail.com>
2022-05-31 19:18:13 +0800

    remove fn_build_ctas

--- SynapseDW/01AdvDW/dbo/Functions/fn_build_ctas.sql
+++ SynapseDW/01AdvDW/dbo/Functions/fn_build_ctas.sql
@@ -1,13 +1,13 @@
-﻿CREATE FUNCTION dbo.fn_build_ctas
-(@name VARCHAR (200), @structure VARCHAR (MAX))
-RETURNS VARCHAR (MAX)
-AS
-BEGIN
-    DECLARE @result AS VARCHAR (MAX);
-    SELECT @result = concat('create table dbo.', @name, ' (
-	', string_agg(CAST (dbo.fn_build_col(colName, colType) AS VARCHAR (MAX)), ','), '
-   )')
-    FROM   OPENJSON (@structure) WITH (colName VARCHAR (200) '$.name', colType VARCHAR (200) '$.type');
-    RETURN @result;
-END
+﻿-- CREATE FUNCTION dbo.fn_build_ctas
+-- (@name VARCHAR (200), @structure VARCHAR (MAX))
+-- RETURNS VARCHAR (MAX)
+-- AS
+-- BEGIN
+--     DECLARE @result AS VARCHAR (MAX);
+--     SELECT @result = concat('create table dbo.', @name, ' (
+-- 	', string_agg(CAST (dbo.fn_build_col(colName, colType) AS VARCHAR (MAX)), ','), '
+--    )')
+--     FROM   OPENJSON (@structure) WITH (colName VARCHAR (200) '$.name', colType VARCHAR (200) '$.type');
+--     RETURN @result;
+-- END
 
